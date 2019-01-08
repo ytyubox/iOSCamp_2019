7115dff047c448c3e95822bfe972fc346f48b604
ytyubox <ytyubox@gmail.com>
2019-01-08 22:23:20 +0800

    [f] 將label置中於容器正中間，並自動調整寬高

--- 游諭內容：UI相關.playground/Pages/Hello, world.xcplaygroundpage/Contents.swift
+++ 游諭內容：UI相關.playground/Pages/Hello, world.xcplaygroundpage/Contents.swift
@@ -3,8 +3,33 @@
 import UIKit
 import PlaygroundSupport
 
-//TODO make a label show "Hello wrold" on view
+
+protocol Declarative { }
+extension Declarative where Self: NSObject {
+	init(configureHandler: (Self) -> Void) {
+		self.init()
+		configureHandler(self)
+	}
+}
+extension NSObject: Declarative { }
+
 class MyTableview:UIViewController{
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		let view = UIView(){
+			$0.backgroundColor = .white
+			$0.frame = UIScreen.main.bounds
+		}
+		let label = UILabel() {
+			$0.text = "Hello, world!"
+			$0.font = $0.font.withSize(30)
+			$0.sizeToFit()
+		}
+		self.view = view
+		label.center = view.center
+		view.addSubview(label)
+		
+	}
 }
 
 PlaygroundPage.current.liveView = MyTableview()
